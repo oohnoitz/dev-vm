3c78bb65c3910de0f2ddbeba8d82a27ae66167a3
Hung Tran <oohnoitz@gmail.com>
2014-02-15 09:48:52 -0600

    created workspace directory for apt. added check for apt/sources modification.

--- deploy/apt.sh
+++ deploy/apt.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+mkdir -p /opt/deploy/workspace
 cd /opt/deploy/workspace
 apt-get install -y netselect-apt grep mawk
 rm -rf /opt/deploy/workspace/sources.list
 netselect-apt
-APT_NEW=`cat /opt/deploy/workspace/sources.list | grep ^deb | awk '{print $2}' | head -1`
-APT_OLD=`cat /etc/apt/sources.list | grep ^deb | awk '{print $2}' | head -1`
-sed -i "s,${APT_OLD},${APT_NEW},g" /etc/apt/sources.list
-apt-get update
+
+if [ -f /opt/deploy/workspace/sources.list ] ; then
+  APT_NEW=`cat /opt/deploy/workspace/sources.list | grep ^deb | awk '{print $2}' | head -1`
+  APT_OLD=`cat /etc/apt/sources.list | grep ^deb | awk '{print $2}' | head -1`
+  sed -i "s,${APT_OLD},${APT_NEW},g" /etc/apt/sources.list
+  apt-get update
+fi
