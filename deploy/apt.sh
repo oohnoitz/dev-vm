fec3f8a8962e54d23bdbe2c5b4b4ebafd12b5c38
Hung Tran <oohnoitz@gmail.com>
2014-02-15 11:34:16 -0600

    forced netselect to use certain push-primary mirrors

--- deploy/apt.sh
+++ deploy/apt.sh
@@ -3,12 +3,10 @@
 mkdir -p /opt/deploy/workspace
 cd /opt/deploy/workspace
 apt-get install -y netselect-apt grep mawk
-rm -rf /opt/deploy/workspace/sources.list
-netselect-apt
+rm -rf /opt/deploy/workspace/apt-mirrors
 
-if [ -f /opt/deploy/workspace/sources.list ] ; then
-  APT_NEW=`cat /opt/deploy/workspace/sources.list | grep ^deb | awk '{print $2}' | head -1`
-  APT_OLD=`cat /etc/apt/sources.list | grep ^deb | awk '{print $2}' | head -1`
-  sed -i "s,${APT_OLD},${APT_NEW},g" /etc/apt/sources.list
-  apt-get update
-fi
+tr '\n' ' ' < /vagrant/deploy/etc/apt-mirrors > /opt/deploy/workspace/apt-mirrors
+APT_NEW=`netselect -s 1 -vv $(cat /opt/deploy/workspace/apt-mirrors) | awk '{print $2}' | head -1`
+APT_OLD=`cat /etc/apt/sources.list | grep ^deb | awk '{print $2}' | head -1`
+sed -i "s,${APT_OLD},${APT_NEW},g" /etc/apt/sources.list
+apt-get update
